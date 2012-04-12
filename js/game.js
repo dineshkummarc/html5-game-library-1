038bc0faf5446aaa59339dcb9b22b495ff8df29d
Mat Scales <mat@netcopy.co.uk>
2012-04-12 10:12:26 +0100

    Some example sounds and sprites.
    Updated game.js to test out the things I've added

--- js/game.js
+++ js/game.js
@@ -3,26 +3,33 @@ if( window.Wibbly == null ) window.Wibbly = {};
 Wibbly.game = {
 	'canvas': null,
 	'audio': null,
+	'input': null,
 	'level': null,
-	'player': {},
+	'player': null,
 	'reset': function()
 	{
 		this.canvas = null;
 		this.audio = null;
+		this.input = null;
 		this.level = null;
-		this.player = {};
+		this.player = null;
 	},
 	'start': function()
 	{
 		this.canvas = new Wibbly.Canvas( document.getElementById( "game" ) );
 		this.audio = new Wibbly.Audio();
+		this.input = new Wibbly.Input();
 		this.level = Wibbly.levels[ 0 ];
 		this.loadSprites();
 		this.loadSounds();
+		this.player = new Wibbly.Player();
+		this.player.moveTo( this.level.spawnPoint );
 		this.tick();
 	},
 	'tick': function()
 	{
+//		console.log( 1000 / ( new Date().getTime() - this.lastFrame ) );
+//		this.lastFrame = new Date().getTime();
 		var self = this;
 		requestAnimationFrame( function(){ self.tick() } );
 		this.update();
@@ -31,21 +38,63 @@ Wibbly.game = {
 	'update': function()
 	{
 		// Get input, move npcs, etc.
+		for( var key in this.input.keys )
+		{
+			switch( key )
+			{
+				case "32": // [Space]
+				{
+					this.audio.play( "low-roar" );
+					delete this.input.keys[ key ];
+					break;
+				}
+
+				case "65": // A
+				{
+					this.player.x--;
+					break;
+				}
+
+				case "68": // D
+				{
+					this.player.x++;
+					break;
+				}
+
+				case "83": // S
+				{
+					this.player.y++;
+					break;
+				}
+
+				case "87": // W
+				{
+					this.player.y--;
+					break;
+				}
+
+				default:
+				{
+					console.log( String.fromCharCode( key ) + " - " + key );
+				}
+			}
+		}
 	},
 	'draw': function()
 	{
 		// Step animation frames and then send all objects to be drawn to the canvas wrapper
 		this.canvas.drawLevel( this.level );
-		var objects = [];
+		var objects = [ this.player ];
 		this.canvas.drawObjects( objects );
 	},
 	'loadSprites': function()
 	{
-		var sprites = [ "void", "grass" ];
+		var sprites = [ "void", "grass", "ball" ];
 
 		for( var index in sprites )
 		{
-			this.canvas.makeSprite( index, document.getElementById( sprites[ index ] ) );
+			var sprite = sprites[ index ]
+			this.canvas.makeSprite( index, sprite, document.getElementById( sprite ) );
 		}
 	},
 	'loadSounds': function()
